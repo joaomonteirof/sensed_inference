1c4920b1660484216d1010692a0748ed63d3d599
joaomonteirof <joaomonteirof@gmail.com>
2018-03-07 14:49:06 -0500

    first commit

--- train_e.py
+++ train_e.py
@@ -20,6 +20,7 @@ parser.add_argument('--beta1', type=float, default=0.5, metavar='lambda', help='
 parser.add_argument('--beta2', type=float, default=0.999, metavar='lambda', help='Adam beta param (default: 0.999)')
 parser.add_argument('--checkpoint-epoch', type=int, default=None, metavar='N', help='epoch to load for checkpointing. If None, training starts from scratch')
 parser.add_argument('--checkpoint-path', type=str, default=None, metavar='Path', help='Path for checkpointing')
+parser.add_argument('--gen-path', type=str, default=None, metavar='Path', help='Path for generative model')
 parser.add_argument('--data-path', type=str, default='./celebA', metavar='Path', help='Path to data')
 parser.add_argument('--workers', type=int, help='number of data loading workers', default=4)
 parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
@@ -37,7 +38,10 @@ mnist = datasets.MNIST('./data', train=True, download=True, transform=transforms
 train_loader = torch.utils.data.DataLoader(mnist, batch_size=args.batch_size, shuffle=True)
 
 generator = model.Generator(100, [1024, 512, 256, 128], 1).eval()
-encoder = model.Discriminator(1, [128, 256, 512, 1024], 1, optim.Adam, args.lr, (args.beta1, args.beta2)).train()
+encoder = model.Encoder(1, [128, 256, 512, 1024], 100).train()
+
+ckpt_gen = torch.load(args.gen_path, map_location = lambda storage, loc: storage)
+generator.load_state_dict(ckpt_gen['model_state'])
 
 if args.cuda:
 	generator = generator.cuda()
